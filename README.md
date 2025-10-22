b8b06d57c56cb5c4fd8c2132a34b1dacde40762f
lkross06 <lucaskalaniross@gmail.com>
2025-10-22 00:25:30 -0700

    feat: implemented simple GET requeest to connect React front-end to Express back-end server

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Bloop
 
-Run ``npm run dev`` to start localhost server.
+**Janani Acharya, Shrika Andhe, Shayla Kumaresan, Lucas Kalani Ross**
+
+Bloop is an app where users can find public bathrooms in Los Angeles.
+
+## Initialization
+
+Run ``npm install`` to install all dependencies.
+
+## Start Server
+
+Run ``npm run dev`` to concurrently start Vite front-end server and Express.js back-end server.
+
+App will be live at http://localhost:5173/
+
+Close the app with CTRL+C in terminal.
+
+If changes are made to Vite front-end (``/src/*``), the
+app will update automatically. If changes are made to express back-end (``/server.js``, ``/vite.config.js``),
+you will need to restart the server.
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export default function App() {
-  return <div>hello</div>;
+function Button({ onClick }) {
+  return <button onClick={onClick}>/GET/test</button>;
+}
+
+export default function Message(){
+  const [message, setMessage] = useState("");
+
+  function handleClick(){
+    //send request to server proxy --> express backend 
+    fetch("/test")
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
+  }
+
+  return <>
+    <div>
+      <Button onClick={() => handleClick()} />
+      <p>The message from http://localhost:3000/test is: {message}</p>
+    </div>
+  </>
 }
