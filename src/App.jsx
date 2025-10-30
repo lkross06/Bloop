bda9524ca9688893a5f30f559ff20b2fbb8e9cfa
lkross06 <lucaskalaniross@gmail.com>
2025-10-29 19:18:45 -0700

    feat: modularized asynchronous function, added more customizable map options

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,16 @@ import { GoogleMap, LoadScriptNext } from "@react-google-maps/api";
 const privateApiKey = import.meta.env.VITE_GOOGLE_MAPS_KEY;
 const privateMapID = import.meta.env.VITE_GOOGLE_MAPS_ID;
 
+/**
+ * zoom = 0   whole world
+ * zoom = 10  city-wide
+ * zoom = 15  neighborhood
+ * zoom = 20  streets
+ */
+const minimumZoom = 11; //how far you can zoom out (1 = world view, 18 = street views)
+const maximumZoom = 17; //how far you can zoom in
+const defaultZoom = 12;
+
 const containerStyle = {
   width: "100%",
   height: "800px",
@@ -27,6 +37,14 @@ const coords = {
   }
 };
 
+function OpenPopUp(){
+
+}
+
+function Button({handleClick, text}){
+  return <button onClick={handleClick}>{text}</button>
+}
+
 function Map({ mapId }) {
   //the map takes a second to load from the API to we keep references to it
   //instead of just creating a new object for it
@@ -39,50 +57,63 @@ function Map({ mapId }) {
     if (!mapInstance) return;
 
     //wait for the library to be available to use
-    (async () => {
-      const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
-
-      //make the graphics
-      const uclaPin = new PinElement({
-        background: "dodgerblue",
-        borderColor: "blue",
-        glyphColor: "blue", //changes middle circle OR text color
-        glyphText: "UCLA"
-      });
-      const uscPin = new PinElement({
-        background: "red",
-        borderColor: "darkred",
-        glyphColor: "darkred",
-        glyphText: "USC"
-      });
-
-      let printTest = (e) => { console.log("test"); };
-
-      //add to the map
-      const uclaMarker = new AdvancedMarkerElement({
-        map: mapInstance, //connect to map
-        position: coords.ucla,
-        content: uclaPin //connect to graphics
-      });
-
-      //add an event listener
-      uclaMarker.addListener('click', printTest);
-
-      const uscMarker = new AdvancedMarkerElement({
-        map: mapInstance,
-        position: coords.usc,
-        content: uscPin
-      });
-
-      uscMarker.addListener('click', printTest);
-
-    })();
+    
   }, [mapInstance]); //list our map as a dependency that the API can read/write
 
   const onLoad = (map) => {
     mapRef.current = map;
     setMapInstance(map);
   };
+
+  //NOTE: choosing to use JS objects instead of React objects (<AdvancedMarker... />) since this is more of a
+  //"back end" endeavor and easy communication with other external services isn't guranteed if we use React objects.
+  //So the only React object is the map, which handles everything.
+  async function addMarker(uniqueID, map, pinProperties, markerProperties, handleClick){
+    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker"); //the asynchronous part comes in here
+
+    try {
+      //make the graphics
+      const pin = new PinElement(pinProperties);
+
+      //make the actual element
+      const marker = new AdvancedMarkerElement({...markerProperties, map: map, content: pin}); //add the graphics and map
+
+      marker.addListener("click", handleClick);
+
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  addMarker(
+    "UCLA",
+    mapInstance, 
+    {
+    background: "dodgerblue",
+    borderColor: "blue",
+    glyphColor: "blue", //changes middle circle OR text color
+    glyphText: "UCLA"
+    },
+    {
+      position: coords.ucla
+    },
+    () => { console.log("test"); }
+  );
+
+  addMarker(
+    "USC",
+    mapInstance, 
+    {
+    background: "red",
+    borderColor: "darkred",
+    glyphColor: "darkred",
+    glyphText: "USC"
+    },
+    {
+      position: coords.usc
+    },
+    () => { console.log("test"); }
+  );
 
   // https://stackoverflow.com/questions/7950030/can-i-remove-just-the-popup-bubbles-of-pois-in-google-maps-api-v3
   // Here we redefine the set() method.
@@ -101,9 +132,14 @@ function Map({ mapId }) {
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={coords.losAngeles}
-      zoom={11} //let's show all of LA for now
+      zoom={defaultZoom} //let's show all of LA for now
       onLoad={onLoad}
-      options={{ mapId }}
+      options={{
+        mapId,
+        disableDefaultUI: true,
+        minZoom: minimumZoom,   
+        maxZoom: maximumZoom, 
+      }} //disable default buttons (fullscreen, street view, etc.)
     >
       {/* empty children since we create marker via API */}
     </GoogleMap>
