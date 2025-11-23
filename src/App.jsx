14db282299c3bcea4c322f9bf9b7c0ea84870c70
lkross06 <lucaskalaniross@gmail.com>
2025-11-22 17:34:28 -0800

    fix: made each Post success banner unique with timestamp in banner ID, so a user updating a post multiple times in a row does not close an existing banner for a previous update

--- src/App.jsx
+++ src/App.jsx
@@ -709,6 +709,8 @@ function PostCreateForm({ location, onSuccess }){
     
     let postID = null;
 
+    let timestamp = Date.now();
+
     if (existingPostID.current != null){
       postID = existingPostID.current;
       DB.updatePost(
@@ -719,7 +721,7 @@ function PostCreateForm({ location, onSuccess }){
         Math.round(availability),
         Math.round(amenities),
         notes,
-        Date.now()
+        timestamp
       );
     } else {
       postID = DB.createPost(
@@ -729,7 +731,7 @@ function PostCreateForm({ location, onSuccess }){
         Math.round(availability),
         Math.round(amenities),
         notes,
-        Date.now()
+        timestamp
       );
     }
     
@@ -739,7 +741,7 @@ function PostCreateForm({ location, onSuccess }){
     //show success banner for 5s
     closeModal();
     openBanner(
-      "create-" + String(postID),
+      "create-" + String(postID) + "-" + String(timestamp),
       <p>Post created successfully!</p>,
       "mediumseagreen",
       5000
@@ -816,7 +818,7 @@ function PostCreateForm({ location, onSuccess }){
         <span className="slider-value">{notes.length} / {maxNotesLength}</span>
       </div>
 
-      <button type="submit" className="create-form-submit">{submitText}</button>
+      <button type="submit" className="create-form-submit">Create Post</button>
     </form>
   );
 }
